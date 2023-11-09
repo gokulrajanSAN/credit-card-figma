6025c4c02c4eb6307dad13afa5bd6c8dce565007
Gokul-HP <gvarutharaj@gmail.com>
2023-11-09 13:12:13 +0530

    9th

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,18 +9,21 @@ const Form = ({ formValue, handleChange }) => {
             <pre>{JSON.stringify(formValue, undefined, 2)}</pre>
 
             <div>
-                <div className='cardholder'>Cardholder Name</div>
-
-                <input type="text" name="hname" id="" className='for-inbut'
+                <div className='card-holder'>Cardholder Name</div>
+                <input type="text" name="hname" id="" className='cardholder'
+                    required
+                    pattern='\w{3,16}'
                     onChange={handleChange}
-                    value={formValue.hname}
+                    // value={formValue.hname}
                     placeholder="e.g. Jane Appleseed" />
+                    
             </div>
 
             <div>
                 <div className='card-number'>Card Number</div>
-
                 <input className='atmnumber' type="number" name="atmnumber" id=""
+                    required
+                    pattern="[0-9]{4}"
                     onChange={handleChange}
                     // value={formValue.atmnumber}
                     placeholder="e.g. 1234 5678 9123 0000" />
@@ -29,14 +32,16 @@ const Form = ({ formValue, handleChange }) => {
             <div style={{ display: "flex", flexDirection: "row", gap: "1.6rem" }}>
                 <div>
                     <div className="Exp-Date" >Exp. Date (MM/YY)</div>
-
                     <input className='atmmonth' type="number" name="atmmonth" id=""
+                        required
+                        pattern="\d{2,2}"
                         onChange={handleChange}
-                        placeholder="mm"
-                    // value={formValue.atmmonth}
-                    />
+                        // value={formValue.atmmonth}
+                        placeholder="mm" />
 
                     <input className='atmyear' type="number" name="atmyear" id=""
+                        required
+                        pattern="\d{2,2}"
                         onChange={handleChange}
                         // value={formValue.atmyear}
                         placeholder="yy" />
@@ -44,10 +49,11 @@ const Form = ({ formValue, handleChange }) => {
 
                 <div>
                     <div className="cvv" >CVV</div>
-
                     <input className='cvv-num' type="number" name="cvv" id=""
+                        // required
                         onChange={handleChange}
-                        // value={formValue.cvv}
+                        value={formValue.cvv}
+                        pattern=".{3,4}"
                         placeholder="e.g.124" />
                 </div>
             </div>
