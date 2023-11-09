e38e6a72109e2fbb6a219f1940a72c65bf41921a
Gokul-HP <gvarutharaj@gmail.com>
2023-11-09 09:45:57 +0530

    6th

--- src/App.js
+++ src/App.js
@@ -18,52 +18,10 @@ function App() {
 
   return (
     <div className="App">
-      <div className='Group-10' style={{
-        width: "30.1875rem",
-        height: "56.25rem",
-        position: "relative",
-        background: "#21092F",
-        overflow: "hidden"
-      }}>
-        <div className='2' style={{
-          width: "15.5rem",
-          height: "41.0625rem",
-          transform: "rotate(45deg)",
-          borderRadius: "41.0625rem",
-          opacity: "0.8474",
-          background: "#D53AFF",
-          filter: "blur(67.95704650878906px)",
-          transformOrigin: "0 0",
-          top: "-10.2rem",
-          left: "20rem",
-          position: "absolute"
-        }}></div>
-        <div className='4' style={{
-          width: "15.5rem",
-          height: "41.0625rem",
-          transform: "rotate(45deg)",
-          borderRadius: "41.0625rem",
-          opacity: "0.8474",
-          background: "#FF834A",
-          filter: "blur(67.95704650878906px)",
-          transformOrigin: "0 0",
-          top: "-7.25rem",
-          left: "29rem",
-          position: "absolute"
-        }}></div>
-        <div className='3' style={{
-          width: "15.5rem",
-          height: "41.0625rem",
-          transform: "rotate(45deg)",
-          borderRadius: "41.0625rem",
-          opacity: "0.8474",
-          background: "#47A2FF",
-          filter: "blur(67.95704650878906px)",
-          transformOrigin: "0 0",
-          top: "7.5rem",
-          left: "15.2rem",
-          position: "absolute"
-        }}></div>
+      <div className='Group-10'>
+        <div className='arc2' />
+        <div className='arc4' />
+        <div className='arc3' />
       </div>
 
       <div style={{
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,158 +1,58 @@
 import './For.css'
-const Form = ({ formValue, handleChange }) => {   
+const Form = ({ formValue, handleChange }) => {
 
 
     return (
         <form className="for"
-            style={{
-                display: "flex", flexDirection: "column", gap: "1.6rem"
-            }}>
+            style={{ display: "flex", flexDirection: "column", gap: "1.6rem" }}>
+
             <pre>{JSON.stringify(formValue, undefined, 2)}</pre>
+
             <div>
-                <div style={{
-                    color: "var(--Deep-Violet, #21092F)",
-                    fontFeatureSettings: "'clig' off, 'liga' off",
-                    fontFamily: "Space Grotesk",
-                    fontSize: "0.75rem",
-                    fontStyle: "normal",
-                    fontWeight: "500",
-                    lineHeight: "normal",
-                    letterSpacing: "0.125rem",
-                    textTransform: "uppercase",
-                }}>Cardholder Name</div>
+                <div className='cardholder'>Cardholder Name</div>
 
                 <input type="text" name="hname" id="" className='for-inbut'
                     onChange={handleChange}
                     value={formValue.hname}
-                    placeholder="e.g. Jane Appleseed"
-                     />
+                    placeholder="e.g. Jane Appleseed" />
             </div>
 
             <div>
-                <div style={{
-                    color: "var(--Deep-Violet, #21092F)",
-                    fontFeatureSettings: "'clig' off, 'liga' off",
-                    fontFamily: "Space Grotesk",
-                    fontSize: "0.75rem",
-                    fontStyle: "normal",
-                    fontWeight: "500",
-                    lineHeight: "normal",
-                    letterSpacing: "0.125rem",
-                    textTransform: "uppercase",
-                }}>Card Number</div>
+                <div className='card-number'>Card Number</div>
 
-                <input type="number" name="atmnumber" id=""
+                <input className='atmnumber' type="number" name="atmnumber" id=""
                     onChange={handleChange}
                     // value={formValue.atmnumber}
-                    placeholder="e.g. 1234 5678 9123 0000"
-                    style={{
-                        padding: "0.6rem",
-                        margin: "0.6rem 0",
-                        width: "23.8125rem",
-                        height: "2.8125rem",
-                        flexShrink: "0",
-                        borderRadius: "0.5rem",
-                        border: "1px solid var(--Light-Grey, #DFDEE0)",
-                        background: "var(--White, #FFF)",
-                    }} /></div>
+                    placeholder="e.g. 1234 5678 9123 0000" />
+            </div>
 
-
-
-            <div style={{
-                display: "flex", flexDirection: "row", gap: "1.6rem"
-            }}>
+            <div style={{ display: "flex", flexDirection: "row", gap: "1.6rem" }}>
                 <div>
-                    <div className="G1-2-T" style={{
-                        color: "var(--Deep-Violet, #21092F)",
-                        fontFeatureSettings: "'clig' off, 'liga' off",
-                        fontFamily: "Space Grotesk",
-                        fontSize: "0.75rem",
-                        fontStyle: "normal",
-                        fontWeight: "500",
-                        lineHeight: "normal",
-                        letterSpacing: "0.125rem",
-                        textTransform: "uppercase",
-                    }}>Exp. Date (MM/YY)</div>
+                    <div className="Exp-Date" >Exp. Date (MM/YY)</div>
 
-                    <input type="number" name="atmmonth" id=""
+                    <input className='atmmonth' type="number" name="atmmonth" id=""
                         onChange={handleChange}
                         placeholder="mm"
-                        // value={formValue.atmmonth}
-                        style={{
-                            padding: "0.6rem",
-                            margin: "0.6rem 0",
-                            width: "5rem",
-                            height: "2.8125rem",
-                            flexShrink: "0",
-                            borderRadius: "0.5rem",
-                            border: "1px solid var(--Light-Grey, #DFDEE0)",
-                            background: "var(--White, #FFF)",
-                        }} />
+                    // value={formValue.atmmonth}
+                    />
 
-                    <input type="number" name="atmyear" id=""
+                    <input className='atmyear' type="number" name="atmyear" id=""
                         onChange={handleChange}
                         // value={formValue.atmyear}
-                        placeholder="yy"
-                        style={{
-                            padding: "0.6rem",
-                            margin: "0.6rem",
-                            width: "5rem",
-                            height: "2.8125rem",
-                            flexShrink: "0",
-                            borderRadius: "0.5rem",
-                            border: "1px solid var(--Light-Grey, #DFDEE0)",
-                            background: "var(--White, #FFF)",
-                        }} />
+                        placeholder="yy" />
                 </div>
 
                 <div>
-                    <div className="G17-9-t" style={{
-                        color: "var(--Deep-Violet, #21092F)",
-                        fontFeatureSettings: "'clig' off, 'liga' off",
-                        fontFamily: "Space Grotesk",
-                        fontSize: "0.75rem",
-                        fontStyle: "normal",
-                        fontWeight: "500",
-                        lineHeight: "normal",
-                        letterSpacing: "0.125rem",
-                        textTransform: "uppercase",
-                    }}>CVV</div>
+                    <div className="cvv" >CVV</div>
 
-                    <input type="number" name="cvv" id=""
+                    <input className='cvv-num' type="number" name="cvv" id=""
                         onChange={handleChange}
                         // value={formValue.cvv}
-                        placeholder="e.g.124"
-                        style={{
-                            padding: "0.6rem",
-                            margin: "0.6rem 0",
-                            width: "8rem",
-                            height: "2.8125rem",
-                            flexShrink: "0",
-                            borderRadius: "0.5rem",
-                            border: "1px solid var(--Light-Grey, #DFDEE0)",
-                            background: "var(--White, #FFF)",
-                        }}
-                    />
+                        placeholder="e.g.124" />
                 </div>
             </div>
 
-            <button
-                style={{
-                    // width: "23.8125rem",
-                    height: "3.3125rem",
-                    flexShrink: "0",
-                    borderRadius: "0.5rem",
-                    background: "var(--Deep-Violet, #21092F)",
-                    color: "var(--White, #FFF)",
-                    fontFeatureSettings: "'clig' off, 'liga' off",
-                    fontFamily: "Space Grotesk",
-                    fontSize: "1.125rem",
-                    fontStyle: "normal",
-                    fontWeight: "500",
-                    lineHeight: "normal",
-                }}>Confirm</button>
-
-
+            <button className='confirmbtn'>Confirm</button>
         </form >
     );
 }
